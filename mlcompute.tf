09d3583532a45af24bf6f51001553462540b531d
Sean Connelly <johndoe@example.com>
2023-09-21 13:43:08 -0400

    Adding compute

--- mlcompute.tf
+++ mlcompute.tf
@@ -12,21 +12,10 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.1.0.0/24"]
 }
 
-resource "azurerm_machine_learning_compute_cluster" "test" {
-  name                          = "example"
+resource "azurerm_machine_learning_compute_instance" "example" {
+  name                          = "example-${random_string.unique_name.result}"
   location                      = azurerm_resource_group.example.location
-  vm_priority                   = "LowPriority"
-  vm_size                       = "Standard_DS11_v2"
   machine_learning_workspace_id = azurerm_machine_learning_workspace.example.id
-  subnet_resource_id            = azurerm_subnet.example.id
-
-  scale_settings {
-    min_node_count                       = 0
-    max_node_count                       = 1
-    scale_down_nodes_after_idle_duration = "PT30S" # 30 seconds
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
+  virtual_machine_size          = "STANDARD_DS11_V2"
+  subnet_resource_id = azurerm_subnet.example.id
 }
